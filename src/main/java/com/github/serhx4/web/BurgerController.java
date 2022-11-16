f9cebd703b305d706d3d45c95c4d537d48eee797
Serhx4 <111274705+Serhx4@users.noreply.github.com>
2022-11-16 21:50:15 +0200

    Update Burger Controller and templates

--- src/main/java/com/github/serhx4/web/BurgerController.java
+++ src/main/java/com/github/serhx4/web/BurgerController.java
@@ -2,22 +2,23 @@ package com.github.serhx4.web;
 
 import com.github.serhx4.data.UserRepository;
 import com.github.serhx4.domain.Burger;
-import com.github.serhx4.domain.Ingredient;
+import com.github.serhx4.domain.dto.BurgerCreateDto;
 import com.github.serhx4.service.BurgerService;
 import com.github.serhx4.service.IngredientService;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Arrays;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/burger")
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class BurgerController {
 
     private final BurgerService burgerService;
@@ -25,45 +26,41 @@ public class BurgerController {
     private final IngredientService ingredientService;
 
     @ModelAttribute
-    public void addIngredientsToModel(Model model) {
-        Arrays.stream(Ingredient.Type.values())
-                .forEach(type ->
-                        model.addAttribute(type.toString().toLowerCase(), ingredientService.filterByType(type)));
+    public void addIngredients(Model model) {
+        ingredientService.addIngredientsToModel(model);
     }
 
-    @GetMapping("/new_burger")
-    public String addNewBurger(Model model) {
-        model.addAttribute("burger", new Burger());
+    @GetMapping("/new")
+    public String addNewBurger(@ModelAttribute("burger") BurgerCreateDto burger) {
         return "burger_form";
     }
 
-    @GetMapping("/update_burger/{id}")
+    @GetMapping("/update/{id}")
     public String updateBurger(@PathVariable Long id, Model model) {
         model.addAttribute("burger", burgerService.findById(id).orElseGet(Burger::new));
         return "burger_form";
     }
 
-    @GetMapping("/my_burgers")
+    @GetMapping("/my")
     public String showUsersBurgers(Model model,
                                    @AuthenticationPrincipal org.springframework.security.core.userdetails.User user) {
-        model.addAttribute("user", user);
+        model.addAttribute("username", user.getUsername());
         model.addAttribute("burgers", burgerService.findAllByUsername(user.getUsername()));
         return "my_burgers";
     }
 
-    @PostMapping("/add_burger")
-    public String addBurger(@Valid Burger burger, Errors errors,
-                            @AuthenticationPrincipal org.springframework.security.core.userdetails.User user) {
+    @PostMapping("/new")
+    public String addBurger(@Valid @ModelAttribute("burger") BurgerCreateDto burger, Errors errors) {
+        if(errors.hasErrors()) {
+            return "burger_form";
+        }
 
-        if(errors.hasErrors()) return "burger_form";
-
-        burgerService.save(burger, userRepository.findById(user.getUsername()).orElse(null));
-        return "redirect:/burger/my_burgers";
+        return "redirect:/menu/" + burgerService.save(burger).getId();
     }
 
     @PostMapping("/delete")
     public String deleteBurger(@RequestParam("id") Long id) {
         burgerService.deleteById(id);
-        return "redirect:/burger/my_burgers";
+        return "redirect:/burger/my";
     }
 }
