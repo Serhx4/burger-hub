28786d5edccab914d51cd6a62d924318198b5f7f
Serhx4 <111274705+Serhx4@users.noreply.github.com>
2023-01-30 14:08:23 +0000

    Implement @Builder pattern for entities. Update DevelopmentConfig preloader

--- src/main/java/com/github/serhx4/DevelopmentConfig.java
+++ src/main/java/com/github/serhx4/DevelopmentConfig.java
@@ -5,7 +5,6 @@ import com.github.serhx4.data.IngredientRepository;
 import com.github.serhx4.data.PromoCodeRepository;
 import com.github.serhx4.data.UserRepository;
 import com.github.serhx4.domain.*;
-import com.github.serhx4.service.ShippingInfoService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static com.github.serhx4.domain.Ingredient.Type.*;
 
@@ -24,38 +24,39 @@ public class DevelopmentConfig {
     @Bean
     public CommandLineRunner preLoader(final IngredientRepository ingredientRepo,
                                        final BurgerRepository burgerRepo,
-                                       UserRepository repo, ShippingInfoService shippingRepo, PromoCodeRepository promoCodeRepo,
+                                       UserRepository repo, PromoCodeRepository promoCodeRepo,
                                        PasswordEncoder encoder) {
         return strings -> {
 
-            ShippingInfo chefShippingInfo = new ShippingInfo();
-            chefShippingInfo.setFirstName("Chef");
-            chefShippingInfo.setLastName("Burger Hub");
-            chefShippingInfo.setPhone("+12334495788");
-            chefShippingInfo.setCountry("USA");
-            chefShippingInfo.setState("California");
-            chefShippingInfo.setCity("San Francisco");
-            chefShippingInfo.setStreet("123 Main St");
-            chefShippingInfo.setApartment("11 Apt");
-            chefShippingInfo.setZip("123");
-            //chefShippingInfo = shippingRepo.save(chefShippingInfo);
+            ShippingInfo chefShippingInfo = ShippingInfo.builder()
+                    .firstName("Chef")
+                    .lastName("Burger Hub")
+                    .phone("+12334495788")
+                    .country("USA")
+                    .state("California")
+                    .city("San Francisco")
+                    .street("123 Main St")
+                    .apartment("11 Apt")
+                    .zip("123")
+                    .build();
 
-            User burgerChef =
-                    new User("chef", "chef@burgerhub.com",
-                            encoder.encode("p"), null, null, null);
-            //chefShippingInfo.setUser(burgerChef);
-            burgerChef.setShippingInfo(chefShippingInfo);
+            User burgerChef = User.builder()
+                    .username("chef")
+                    .email("chef@burgerhub.com")
+                    .password(encoder.encode("p"))
+                    .shippingInfo(chefShippingInfo)
+                    .build();
             repo.save(burgerChef);
 
             // Ingredients
             final Ingredient sesameSeedBun = new Ingredient("SBUN", "Sesame seed bun", BigDecimal.valueOf(0.19),
-                            "image/ingredients/seasame_bun.png", BREAD);
+                    "image/ingredients/seasame_bun.png", BREAD);
             final Ingredient toastedBread = new Ingredient("BRED", "Toasted bread", BigDecimal.valueOf(0.09),
                     "image/ingredients/bread.png", BREAD);
             final Ingredient beefPatty = new Ingredient("BEEF", "Beef patty", BigDecimal.valueOf(2.5),
-                            "image/ingredients/beef_patty.png", PROTEIN);
+                    "image/ingredients/beef_patty.png", PROTEIN);
             final Ingredient lambPatty = new Ingredient("LAMB", "Lamb patty", BigDecimal.valueOf(2.5),
-                            "image/ingredients/lamb_patty.png", PROTEIN);
+                    "image/ingredients/lamb_patty.png", PROTEIN);
             final Ingredient salmon = new Ingredient("SALM", "Salmon fillet", BigDecimal.valueOf(3.5),
                     "image/ingredients/salmon.png", PROTEIN);
             final Ingredient shrimps = new Ingredient("SHRM", "Shrimp patty", BigDecimal.valueOf(3.5),
@@ -66,7 +67,7 @@ public class DevelopmentConfig {
                     "image/ingredients/tomatoes.png", VEGGIES);
             final Ingredient crispLettuce = new Ingredient("LETC", "Crisp lettuce", BigDecimal.valueOf(0.2),
                     "image/ingredients/lettuce.png", VEGGIES);
-            final Ingredient redOnions = new Ingredient("ONIO","Sliced red onions", BigDecimal.valueOf(0.1),
+            final Ingredient redOnions = new Ingredient("ONIO", "Sliced red onions", BigDecimal.valueOf(0.1),
                     "image/ingredients/red_onion.png", VEGGIES);
             final Ingredient cucumber = new Ingredient("CUCM", "Sliced cucumber", BigDecimal.valueOf(0.1),
                     "image/ingredients/cucumber.png", VEGGIES);
@@ -117,126 +118,137 @@ public class DevelopmentConfig {
             ingredientRepo.save(friedEgg);
 
             // Burgers
-            Burger hamburger = new Burger();
-            hamburger.setName("Hamburger");
-            hamburger.setImageUri("image/burger/hamburger.png");
-            hamburger.setUser(burgerChef);
-            hamburger.setIngredients(new ArrayList<Ingredient>() {{
-                add(sesameSeedBun);
-                add(beefPatty);
-                add(crunchyPickles);
-                add(yellowMustard);
-                add(tomatoKetchup);
-            }});
 
-            Burger cheeseburger = new Burger();
-            cheeseburger.setName("Cheeseburger");
-            cheeseburger.setImageUri("image/burger/cheeseburger.png");
-            cheeseburger.setUser(burgerChef);
-            cheeseburger.setIngredients(new ArrayList<Ingredient>() {{
-                add(sesameSeedBun);
-                add(beefPatty);
-                add(cheddarCheese);
-                add(crunchyPickles);
-                add(yellowMustard);
-                add(tomatoKetchup);
-            }});
-
-            Burger baconCheeseburger = new Burger();
-            baconCheeseburger.setName("Bacon Cheeseburger");
-            baconCheeseburger.setImageUri("image/burger/bacon cheeseburger.png");
-            baconCheeseburger.setUser(burgerChef);
-            baconCheeseburger.setIngredients(new ArrayList<Ingredient>(){{
-                add(sesameSeedBun);
-                add(beefPatty);
-                add(cheddarCheese);
-                add(crunchyPickles);
-                add(yellowMustard);
-                add(tomatoKetchup);
-                add(crispyBacon);
-            }});
-
-            Burger whopper = new Burger();
-            whopper.setName("Whopper");
-            whopper.setImageUri("image/burger/whopper.png");
-            whopper.setUser(burgerChef);
-            whopper.setIngredients(new ArrayList<Ingredient>() {{
-                add(sesameSeedBun);
-                add(beefPatty);
-                add(juicyTomatoes);
-                add(crispLettuce);
-                add(creammyMayonnaise);
-                add(tomatoKetchup);
-                add(crunchyPickles);
-                add(redOnions);
-            }});
-
-            Burger salmonBurger = new Burger();
-            salmonBurger.setName("Wild Salmon");
-            salmonBurger.setImageUri("image/burger/salmon.png");
-            salmonBurger.setUser(burgerChef);
-            salmonBurger.setIngredients(new ArrayList<Ingredient>() {{
-                add(sesameSeedBun);
-                add(salmon);
-                add(friedEgg);
-                add(sweetPepper);
-                add(cucumber);
-                add(redOnions);
-                add(creammyMayonnaise);
-            }});
-
-            Burger shrimpBurger = new Burger();
-            shrimpBurger.setName("Shrimp Burger");
-            shrimpBurger.setImageUri("image/burger/shrimp.png");
-            shrimpBurger.setUser(burgerChef);
-            shrimpBurger.setIngredients(new ArrayList<Ingredient>() {{
-                add(sesameSeedBun);
-                add(shrimps);
-                add(friedEgg);
-                add(sweetPepper);
-                add(crispLettuce);
-                add(bbqSauce);
-            }});
-
+            Burger hamburger = Burger.builder()
+                    .name("Hamburger")
+                    .imageUri("image/burger/hamburger.png")
+                    .user(burgerChef)
+                    .ingredients(new ArrayList<>(Arrays.asList(
+                            sesameSeedBun,
+                            beefPatty,
+                            crunchyPickles,
+                            yellowMustard,
+                            tomatoKetchup
+                    )))
+                    .build();
             burgerRepo.save(hamburger);
+
+            Burger cheeseburger = Burger.builder()
+                    .name("Cheeseburger")
+                    .imageUri("image/burger/cheeseburger.png")
+                    .user(burgerChef)
+                    .ingredients(new ArrayList<>(Arrays.asList(
+                            sesameSeedBun,
+                            beefPatty,
+                            cheddarCheese,
+                            crunchyPickles,
+                            yellowMustard,
+                            tomatoKetchup
+                    )))
+                    .build();
             burgerRepo.save(cheeseburger);
+
+            Burger baconCheeseburger = Burger.builder()
+                    .name("Bacon Cheeseburger")
+                    .imageUri("image/burger/bacon cheeseburger.png")
+                    .user(burgerChef)
+                    .ingredients(new ArrayList<>(Arrays.asList(
+                            sesameSeedBun,
+                            beefPatty,
+                            cheddarCheese,
+                            crunchyPickles,
+                            yellowMustard,
+                            tomatoKetchup,
+                            crispyBacon
+                    )))
+                    .build();
             burgerRepo.save(baconCheeseburger);
+
+            Burger whopper = Burger.builder()
+                    .name("Whopper")
+                    .imageUri("image/burger/whopper.png")
+                    .user(burgerChef)
+                    .ingredients(new ArrayList<>(Arrays.asList(
+                            sesameSeedBun,
+                            beefPatty,
+                            juicyTomatoes,
+                            crispLettuce,
+                            creammyMayonnaise,
+                            tomatoKetchup,
+                            crunchyPickles,
+                            redOnions
+                    )))
+                    .build();
             burgerRepo.save(whopper);
+
+            Burger salmonBurger = Burger.builder()
+                    .name("Wild Salmon")
+                    .imageUri("image/burger/salmon.png")
+                    .user(burgerChef)
+                    .ingredients(new ArrayList<>(Arrays.asList(
+                            sesameSeedBun,
+                            salmon,
+                            friedEgg,
+                            sweetPepper,
+                            cucumber,
+                            redOnions,
+                            creammyMayonnaise
+                    )))
+                    .build();
             burgerRepo.save(salmonBurger);
+
+            Burger shrimpBurger = Burger.builder()
+                    .name("Shrimp Burger")
+                    .imageUri("image/burger/shrimp.png")
+                    .user(burgerChef)
+                    .ingredients(new ArrayList<>(Arrays.asList(
+                            sesameSeedBun,
+                            shrimps,
+                            friedEgg,
+                            sweetPepper,
+                            crispLettuce,
+                            bbqSauce
+                    )))
+                    .build();
             burgerRepo.save(shrimpBurger);
 
             // Promo Codes
 
-            PromoCode iLoveMac = new PromoCode();
-            iLoveMac.setCode("#ILOVEMAC");
-            iLoveMac.setDiscount(BigDecimal.valueOf(0.05));
-
-            PromoCode mcDonaldsForever = new PromoCode();
-            mcDonaldsForever.setCode("#MCDONALDSFOREVER");
-            mcDonaldsForever.setDiscount(BigDecimal.valueOf(0.01));
-            PromoCode iLoveBurgerHub = new PromoCode();
-            iLoveBurgerHub.setCode("#ILOVEBURGERHUB");
-            iLoveBurgerHub.setDiscount(BigDecimal.valueOf(0.2));
-
-            PromoCode iLoveBurgers = new PromoCode();
-            iLoveBurgers.setCode("#ILOVEBURGERS");
-            iLoveBurgers.setDiscount(BigDecimal.valueOf(0.1));
-
-            PromoCode iWantToEat = new PromoCode();
-            iWantToEat.setCode("#IWANTTOEAT");
-            iWantToEat.setDiscount(BigDecimal.valueOf(0.3));
-
-            PromoCode giveMeSomeMeal = new PromoCode();
-            giveMeSomeMeal.setCode("#GIVEMESOMEMEAL");
-            giveMeSomeMeal.setDiscount(BigDecimal.valueOf(0.5));
-
+            PromoCode iLoveMac = PromoCode.builder()
+                    .code("#ILOVEMAC")
+                    .discount(BigDecimal.valueOf(0.05))
+                    .build();
             promoCodeRepo.save(iLoveMac);
-            promoCodeRepo.save(mcDonaldsForever);
-            promoCodeRepo.save(iLoveBurgerHub);
-            promoCodeRepo.save(iLoveBurgers);
-            promoCodeRepo.save(iWantToEat);
-            promoCodeRepo.save(giveMeSomeMeal);
 
+            PromoCode mcDonaldsForever = PromoCode.builder()
+                    .code("#MCDONALDSFOREVER")
+                    .discount(BigDecimal.valueOf(0.01))
+                    .build();
+            promoCodeRepo.save(mcDonaldsForever);
+
+            PromoCode iLoveBurgerHub = PromoCode.builder()
+                    .code("#ILOVEBURGERHUB")
+                    .discount(BigDecimal.valueOf(0.2))
+                    .build();
+            promoCodeRepo.save(iLoveBurgerHub);
+
+            PromoCode iLoveBurgers = PromoCode.builder()
+                    .code("#ILOVEBURGERS")
+                    .discount(BigDecimal.valueOf(0.1))
+                    .build();
+            promoCodeRepo.save(iLoveBurgers);
+
+            PromoCode iWantToEat = PromoCode.builder()
+                    .code("#IWANTTOEAT")
+                    .discount(BigDecimal.valueOf(0.3))
+                    .build();
+            promoCodeRepo.save(iWantToEat);
+
+            PromoCode giveMeSomeMeal = PromoCode.builder()
+                    .code("#GIVEMESOMEMEAL")
+                    .discount(BigDecimal.valueOf(0.5))
+                    .build();
+            promoCodeRepo.save(giveMeSomeMeal);
         };
     }
 
--- src/main/java/com/github/serhx4/domain/Ingredient.java
+++ src/main/java/com/github/serhx4/domain/Ingredient.java
@@ -12,6 +12,7 @@ import java.util.Objects;
 @Getter
 @Setter
 @ToString
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
